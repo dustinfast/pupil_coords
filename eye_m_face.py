0a68dd915dce67e203cf1c25e3fad658a4c307ad
dustinfast <dustin.fast@hotmail.com>
2018-06-17 15:24:52 -0800

    Began implimenting hill climb for image optimization -DF

--- eye_m_face.py
+++ eye_m_face.py
@@ -4,17 +4,19 @@
 import sys
 import cv2
 import numpy as np
+from random import randint
 
-NOSE_MODEL = 'models/haarcascade_nose.xml'
-EYE_MODEL = 'models/haarcascade_eye.xml'
+N_MODEL = 'models/haarcascade_nose.xml'
+E_MODEL = 'models/haarcascade_eye.xml'
 
 class Face(object):
     """ Representation of a face in the form of its nose and pupil coords.
     """
-    def __init__(self, frame=None, eye_model=EYE_MODEL, nose_model=NOSE_MODEL):
+    def __init__(self, eye_model=E_MODEL, nose_model=N_MODEL):
         self.eye_model = cv2.CascadeClassifier(eye_model)
         self.nose_model = cv2.CascadeClassifier(nose_model)
-        self.cam = cv2.VideoCapture(0)  # ini camera
+        self.kern = np.ones((5, 5), np.uint8)   # frame transform kernal
+        self.cam = cv2.VideoCapture(0)          # ini camera
 
         # TODO: Do I need indiv coords
         self.nose_x = 0  
@@ -23,16 +25,14 @@ class Face(object):
         self.l_eye_y = 0
         self.r_eye_x = 0
         self.r_eye_y = 0
-        self._points = []           # [ (l_eye_xy), (r_eye_xy), (nose_xy) ]
-        self.fail_count = 0
-        self.frame_count = 0
-        self.max_count = sys.maxint
+        self._points = []           # [(le_x, le_y), (re_x, re_y), (n_x, n_y)]
+        self.fails = 0              # Classificatoin failures
+        self.frame_count = 0        # Classification attempts
+        self.max_frames = 5         # Attempts before determine accuracy
+        self.acc = 0                # Accuracy, as a percentage
+        self.prev_acc = 0           # Prev accuracy percentage
+        self.mask = 0               # Optimation mask
         # TODO Depth, etc?
-        
-        if frame is not None:
-            self.frame = frame
-            self.frame_opt = self._img_opt()
-            self._set_features()
     
     def __str__(self):
         """ Returns a string representation of self.
@@ -47,35 +47,62 @@ class Face(object):
         ret_str = str(self.nose_y)
         return ret_str
 
-    def _update(self, frame):
-        """ Updates the face object with the given frame.
+    def _update(self):
+        """ Updates the face object with the given frame according to the best
+            optimization combination it can find.
         """
-        self.frame = frame
-        self.frame_opt = self._img_opt()
+        _, self.frame = self.cam.read()  # Get cam data
         self._points = []
-
-        self._set_features()
+        self._optimize_frame(self.mask)
+        self._find_features()
+        self._mark_pts()
 
         self.frame_count += 1
-        if self.frame_count == self.max_count:
-            print('Resetting accuracy metrics. Last=%' + str(self.accuracy()))
+        if self.frame_count == self.max_frames:
+            self.prev_acc = self.acc
+            self.acc = int((self.max_frames - self.fails) * 100.0 / self.max_frames)
             self.frame_count = 0
-            self.fail_count = 0
+            self.fails = 0
 
-    def _set_features(self):
-        """ Sets facial feature coordinates based on self.frame_opt.
+            # Do hill climb to find best optimzation if needed
+            print('Curr: ' + str(self.acc) + ', Prev: ' + str(self.prev_acc))
+            if self.acc == 0 or self.acc + 5 < self.prev_acc:
+                print('Looking for new best mask')
+                masks = {}
+                masks[self.acc] = self.mask # Include current mask in results
+
+                # For max_frames diff masks
+                for i in range(self.max_frames):
+                    temp_mask = self._optimize_frame()
+                    fails = 0
+                    print('Temp mask: ' + str("{0:b}".format(self.mask)))
+                    
+                    # Try curr mask on max_frames frames
+                    for j in range(self.max_frames):
+                        _, self.frame = self.cam.read()  # Get cam data
+                        if not self._find_features():
+                            fails += 1
+                    acc = int((self.max_frames - fails) * 100.0 / self.max_frames)
+                    masks[acc] = temp_mask
+                best = sorted(masks.keys())[0]
+                self.mask = masks[best]
+                print('New best: ' + str(best) + str("{0:b}".format(self.mask)))
+
+
+    def _find_features(self):
+        """ Returns True iff facial feature coordinates were found.
             Assumes self.frame_opt is not None.
         """
         def _fail():
-            self.fail_count += 1
+            self.fails += 1
             self._reset_coords()
-            # print(str(self.fail_count) + (': no eyes'))  # debug
+            # print(str(self.fails) + (': no eyes'))  # debug
 
         # Detect facial features
         eyes = self.eye_model.detectMultiScale(self.frame_opt, 1.3, 5)
         nose = self.nose_model.detectMultiScale(self.frame_opt, 1.3, 5)
 
-        # Proceed if both eyes detected
+        # Fail if two eyes not deted
         if not len(eyes) == 2:  
             _fail()
             return
@@ -93,7 +120,7 @@ class Face(object):
 
         if eye_1_x < eye_2_x:  # ensure correct eye gets assigned
             if (eye_2_x - eye_1_x) < 20:
-                _fail()  # eyes too close together
+                _fail()  # if eyes unreasonably close together
                 return
             self.l_eye_x = eye_1_x
             self.l_eye_y = eye_1_y
@@ -101,7 +128,7 @@ class Face(object):
             self.r_eye_y = eye_2_y
         else:
             if (eye_1_x - eye_2_x) < 20:
-                _fail()  # eyes too close together
+                _fail()  # if eyes unreasonably close together
                 return
             self.l_eye_x = eye_2_x
             self.l_eye_y = eye_2_y
@@ -117,8 +144,7 @@ class Face(object):
             self._points.append((self.nose_x, self.nose_y))
             break  # Ensure only one nose
 
-        # Mark features on self.frame
-        self._mark_pts()
+        return True
 
     def _mark_pts(self):
         """ Marks self.frame with the facial features.
@@ -141,36 +167,64 @@ class Face(object):
         self.r_eye_y = 0
         self._points = []
 
-    def _img_opt(self):
+    def _optimize_frame(self, mask=None):
         """ Returns a version of self.frame optimzed for classification.
-            Transform is from: https://docs.opencv.org/3.1.0/d5/daf/
-            tutorial_py_histogram_equalization.html
+            If not mask, generates its own mask randomly
         """
-        # TODO Optimize heuristics
-        # TODO: check heuristics and adjust based on lighting
-        # TODO: Erosion? https://docs.opencv.org/3.0-beta/doc/py_tutorials/
-        # py_imgproc/py_morphological_ops/py_morphological_ops.html
         frame = cv2.cvtColor(self.frame, cv2.COLOR_RGB2GRAY)
+
+        if False:  # debug
+            frame = cv2.erode(frame, self.kern, iterations=1)  # Erode foreground
+            return frame
+
+        if not mask:
+            mask = randint(0, 7)  # TODO: tilt prop towards 0
+
+        # If mask<5, bal img before opts, if>5, do it after. If==5, skip bal.
+        if mask < 5:
+            frame = self._balance(frame)
+
+        if mask & 0:
+            frame = cv2.erode(frame, self.kern, iterations=1)
+        if mask & 1:
+            frame = cv2.erode(frame, self.kern, iterations=2)
+        if mask & 2:
+            frame = cv2.dilate(frame, self.kern, iterations=1)
+        if mask & 3:
+            frame = cv2.dilate(frame, self.kern, iterations=2)
+        if mask & 4:
+            frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, self.kern)
+        if mask & 5:
+            frame = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, self.kern)
+        if mask & 6: 
+            frame = cv2.morphologyEx(frame, cv2.MORPH_GRADIENT, self.kern)
+        if mask & 7:
+            # do nothing
+            pass
+
+        if mask > 5:
+            frame = self._balance(frame)
+
+        self.frame_opt = frame
+
+        return mask
+
+    def _balance(self, frame):
+        """ Given a frame, returns a version with milder gradients.
+        """
         hist, _ = np.histogram(frame.flatten(), 256, [0, 256])
         cdf = hist.cumsum()
         cdf_normalized = cdf * hist.max() / cdf.max()
         cdf_m = np.ma.masked_equal(cdf, 0)
         cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
         cdf = np.ma.filled(cdf_m, 0).astype('uint8')
-
         return cdf[frame]
 
-    def accuracy(self):
-        """ Returns an int representing the percent of successfull eye finds.
-        """
-        return (self.frame_count - self.fail_count) * 100.0 / self.frame_count
-
     def run_single(self):
-        """ Gets and processes a single frame, then returns the feature points.
+        """ Gets and processes a single frame, then returns the feature points:
+            [ (l_eye_x, l_eye_y), (r_eye_x, r_eye_y), (nose_x, nose_y)
         """
-        _, frame = self.cam.read()
-        self._update(frame)
-
+        self._update()
         return self._points
 
     def run_live(self):
@@ -178,9 +232,8 @@ class Face(object):
             in a window. Spacebar quits.
         """
         while True:
-            _, frame = self.cam.read()
-            self._update(frame)
-            cv2.imshow('frame', frame)
+            self._update()
+            cv2.imshow('frame', self.frame)
             cv2.imshow('optimzed frame', self.frame_opt)
             if cv2.waitKey(1) == 32:
                 break  # spacebar to quit
