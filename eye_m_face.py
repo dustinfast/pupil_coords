eccd17cb85b05e3aceb4108ceb4914f7a231ffd3
dustinfast <dustin.fast@hotmail.com>
2018-06-17 09:17:33 -0800

    refactored img functions -DF

--- eye_m_face.py
+++ eye_m_face.py
@@ -20,9 +20,7 @@ except:
     raise Exception('Error loading cascade models.')
 
 class Face(object):
-    def __init__(self, frame):
-        self.frame = frame
-        self.frame_mod = frame
+    def __init__(self, frame=None):
         self.nose_x = 0
         self.nose_y = 0
         self.l_eye_x = 0
@@ -30,26 +28,36 @@ class Face(object):
         self.r_eye_x = 0
         self.r_eye_y = 0
         self._points = []
+        self.err_count = 0
         
-        self._set_features()
+        if frame is not None:
+            self.frame = frame
+            self.frame_opt = img_opt(frame)
+            self._set_features()
     
-    def _set_features(self):
-        """ Sets facial feature coordinates based on self.frame.
+    def _update(self, frame):
+        """ Updates the face object with the given frame.
         """
-        if self.frame_mod is None:
-            return
+        self.frame = frame
+        self.frame_opt = img_opt(frame)
+        self._points = []
 
-        # Optimize frame for classification # TODO: Anything else?
-        frame_mod = cv2.cvtColor(self.frame, cv2.COLOR_RGB2GRAY)  # frame->rgb
-        frame_mod = balance_brightness(frame_mod)
-        self.frame_mod = frame_mod 
+        self._set_features()
 
+    def _set_features(self):
+        """ Sets facial feature coordinates based on self.frame_opt.
+            Assumes self.frame_opt is not None.
+        """
         # Detect facial features
-        eyes = g_eye_model.detectMultiScale(frame_mod, 1.3, 5)
-        nose = g_nose_model.detectMultiScale(frame_mod, 1.3, 5)
+        eyes = g_eye_model.detectMultiScale(self.frame_opt, 1.3, 5)
+        nose = g_nose_model.detectMultiScale(self.frame_opt, 1.3, 5)
 
         # Proceed if both eyes detected
         if not len(eyes) == 2:  
+            print(str(self.err_count) + (': no eyes'))
+            self.err_count += 1
+            if self.err_count > 200:
+                exit()
             return
 
         # Extract each coord and assign to self
@@ -64,17 +72,19 @@ class Face(object):
                 eye_2_y = eye_y
 
         if eye_1_x < eye_2_x:  # ensure correct eye gets assigned
+            if (eye_2_x - eye_1_x) < 20:
+                return  # eyes too close
             self.l_eye_x = eye_1_x
             self.l_eye_y = eye_1_y
             self.r_eye_x = eye_2_x
             self.r_eye_y = eye_2_y
         else:
+            if (eye_1_x - eye_2_x) < 20:
+                return  # eyes too close
             self.l_eye_x = eye_2_x
             self.l_eye_y = eye_2_y
             self.r_eye_x = eye_1_x
             self.r_eye_y = eye_1_y
-
-        # TODO: Ensure eyes are reasonably far apart
 
         self._points.append((self.l_eye_x, self.l_eye_y))
         self._points.append((self.r_eye_x, self.r_eye_y))
@@ -105,7 +115,7 @@ class Face(object):
         if frame is None:
             frame = self.frame
         for p in self._points:
-            cv2.circle(frame, (p[0], p[1]), 1, (0, 0, 255), 1)
+            cv2.circle(frame, (p[0], p[1]), 1, (0, 255, 0), 1)
 
         # debug
         # cv2.circle(frame, (self.l_eye_x + 1, self.l_eye_y + 1), 1, (0, 0, 255), 1)
@@ -113,10 +123,22 @@ class Face(object):
 
         return frame
 
-def get_cropped(frame, x, y, w, z):
+def img_crop(frame, x, y, w, z):
+    """ Given an image frame and x, y, w, z crop coords, returns cropped img.
+    """
     return frame[y:y + z, x:x + w]  # [Y1:Y2, X1:X2]
 
-def balance_brightness(frame):
+
+def img_opt(frame):
+    """ given an image frame, returns an optimzed version for classification.
+    """
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    frame = img_bal_brightness(frame)  # TODO: Find optimal
+
+    return frame
+
+
+def img_bal_brightness(frame):
     """ Apply transform to even out brightness.
         From: https://docs.opencv.org/3.1.0/d5/daf/
         tutorial_py_histogram_equalization.html
@@ -132,6 +154,7 @@ def balance_brightness(frame):
 
 if __name__ == '__main__':
     print('Starting...')
+    face = Face()
     cam = cv2.VideoCapture(0)
 
     while True:
@@ -139,12 +162,12 @@ if __name__ == '__main__':
         if MIRROR_IMG:
             frame = cv2.flip(frame, 1)
         
-        face = Face(frame)
+        face._update(frame)
         frame = face.get_marked()
         # frame = np.hstack((frame, face.pframe))  # put imgs side by side
 
         cv2.imshow('Output', frame)
-        # cv2.imshow('Output2', face.get_marked(face.frame_mod))
+        cv2.imshow('Output2', face.get_marked(face.frame_opt))
         time.sleep(.1)
 
         if cv2.waitKey(1) == 32:
