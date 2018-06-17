1db861eb9ac124320a772be0691b6573497a389c
dustinfast <dustin.fast@hotmail.com>
2018-06-17 08:05:46 -0800

    mouse coords returning OK -DF

--- eye_m_mouse.py
+++ eye_m_mouse.py
@@ -5,46 +5,44 @@ __author__ = "Dustin Fast (dustin.fast@outlook.com)"
 
 import time
 import threading
+import win32api
 
 class mouse(object):
-	""" TODO
-	"""
+    """ TODO
+    """
 
-	def __init__(self, mouse_hz, learn_hz, ann):
-		""" TODO
-		"""
-		self.ann = None
-		self.threads_on = None
+    def __init__(self, mouse_hz):
+        """ TODO
+        """
+        self.threads_on = None
+        self.mouse_thread = threading.Thread(target=self._mouse)
+        self.mouse_hz = mouse_hz
 
-		self.learn_thread = threading.Thread(target=self._mouse)
-		self.mouse_hz = mouse_hz
+    # Threaded
+    def _mouse(self):
+        """ TODO
+        """
+        pass
 
-	# Threaded
-	def _mouse(self):
-		""" TODO
-		"""
-		pass
+    def start(self):
+        """ Starts the mousing threads.
+        """
+        print('eye_m is mousing...')
 
-	def start(self):
-		""" Starts the mousing threads.
-		"""
-		print('eye_m is mousing...')
-		
+    def stop(self):
+        """ Stop mousing threads.
+        """
+        self.threads_on = None
+        self.mouse_thread.start()
 
-	def stop(self):
-		""" Stop mousing threads.
-		"""
-		self.threads_on = None
+    def status(self):
+        """ Outputs status to console.
+        """
+        if self.mouse_thread.isAlive():
+            print('Mouse Thread: On')
+        else:
+            print('Mouse Thread: Off')
 
-	def status(self):
-		""" Outputs status to console.
-		"""
-		# if self.data_thread.isAlive():
-		# 	print('Data Thread: On')
-		# else:
-		# 	print('Data Thread: Off')
-
-		# if self.learn_thread.isAlive():
-		# 	print('Learning Thread: On')
-		# else:
-		# 	print('Learning Thread: Off')
+	def mouse_pos():
+		x, y = win32api.GetCursorPos()
+		return x, y
